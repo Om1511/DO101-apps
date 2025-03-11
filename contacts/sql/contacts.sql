22b58f736c7909db11e8a625cd5e856297848fe2
Om1511 <om.dongare23@pcu.edu.in>
2025-03-11 13:21:23 +0530

    Update contact.sql

--- contacts/sql/contacts.sql
+++ contacts/sql/contacts.sql
@@ -2,6 +2,7 @@ drop table if exists contact;
 
 create table contact(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null);
 
+
 insert into contact(firstname, lastname, email) values
   ('Bilbo','Baggins','bilbo@theshire.com'),
   ('Frodo','Baggins','frodo@theshire.com'),
